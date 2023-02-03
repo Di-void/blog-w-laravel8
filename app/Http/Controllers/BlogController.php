19b9b1d3b1cbc9697921c2780e503b5c46bb6eb5
DEE <donzydonathan@gmail.com>
2023-02-03 22:53:28 +0100

    feat: generate image path for blog image uploads and store to public disk

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class BlogController extends Controller
 {
@@ -18,6 +20,15 @@ class BlogController extends Controller
             "image" => 'required | image',
             "body" => 'required',
         ]);
+
+        $title = $request->input('title');
+        $slug = Str::slug($title, '-');
+        $user_id = Auth::user()->id;
+        $body = $request->input('body');
+
+        //File upload
+        $imagePath = 'storage/' . $request->file('image')->store('postImages','public');
+
         dd('validation passed, you can now request the input');
     }
 
