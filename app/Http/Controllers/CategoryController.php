ccaaa274a76f29a74dde17918f7b3169d85f7ffe
DEE <donzydonathan@gmail.com>
2023-02-06 17:31:55 +0100

    feat: implement 'delete category' functionality

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -103,6 +103,7 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+        return redirect()->back()->with('status', 'Category Deleted Successfully');
     }
 }
